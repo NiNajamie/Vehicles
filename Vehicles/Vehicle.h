de9a27e3b7bb319d8973efcdd3031971d0a46ff3
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-05-08 11:40:29 -0700

    added methods

--- Vehicles/Vehicle.h
+++ Vehicles/Vehicle.h
@@ -18,5 +18,15 @@
 @property (nonatomic, copy) NSString *modelName;
 @property (nonatomic, assign) NSInteger modelYear;
 
+//Basic operation methods
+- (NSString *) goForward;
+- (NSString *) goBackward;
+- (NSString *) stopMoving;
+- (NSString *) changeGears: (NSString *) newGearName;
+- (NSString *) turn: (NSInteger *) degrees;
+- (NSString *) makeNoise;
+
+
+
 
 @end
